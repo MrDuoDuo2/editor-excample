4a8ad6615269413b0a088900e013fd085f5216f5
MrDuoDuo2 <nmbh1122@126.com>
2025-12-09 17:25:41 +0800

    feat: 初始化编辑器项目

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import {schema} from "prosemirror-schema-basic"
+import {EditorState} from "prosemirror-state"
+import {EditorView} from "prosemirror-view"
+import {undo, redo, history} from "prosemirror-history"
+import {keymap} from "prosemirror-keymap"
+import {baseKeymap} from "prosemirror-commands"
+
+let state = EditorState.create({schema,plugins: [
+  history(),
+  keymap({"Mod-z": undo, "Mod-y": redo}),
+  keymap(baseKeymap)
+]})
+let view = new EditorView(document.body, {
+  state
+})
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // 配置开发服务器
+  server: {
+    port: 3000,
+    open: true
+  }
+})
+
